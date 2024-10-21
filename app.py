b40ed6cbf430793ddddbbe42467bd481754a5b37
shashikant010 <shashikantyadav0503@gmail.com>
2024-10-21 11:33:19 +0530

    added more models

--- app.py
+++ app.py
@@ -14,9 +14,15 @@ CORS(app,origins="*")
 with open('model2.pkl', 'rb') as f:
     model = pickle.load(f)
 
+with open('kms-model2.pkl', 'rb') as f:
+    model2 = pickle.load(f)
+
+with open('year-model.pkl', 'rb') as f:
+    model3 = pickle.load(f)
+
 # Define a route for prediction
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/predict-price', methods=['POST'])
+def predictPrice():
     try:
         # Get the data from the request
         data = request.json
@@ -32,6 +38,44 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)})
 
+
+
+@app.route('/predict-kms', methods=['POST'])
+def predictKms():
+    try:
+        # Get the data from the request
+        data = request.json
+
+        # Convert data to a pandas DataFrame
+        input_df = pd.DataFrame(data)
+
+        # Use the model to make a prediction
+        prediction = model2.predict(input_df)
+
+        return jsonify({'kms_driven': prediction.tolist()})
+        # return jsonify({'kms_driven is fetched':"ok"})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+
+@app.route('/predict-year', methods=['POST'])
+def predictYear():
+    try:
+        # Get the data from the request
+        data = request.json
+
+        # Convert data to a pandas DataFrame
+        input_df = pd.DataFrame(data)
+
+        # Use the model to make a prediction
+        prediction = model3.predict(input_df)
+
+        return jsonify({'year': prediction.tolist()})
+        # return jsonify({'kms_driven is fetched':"ok"})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
 # Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
